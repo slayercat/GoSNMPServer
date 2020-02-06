921e86b113b37a2592d113d0a87b78a595453acf
lilinzhe <slayercat.subscription@gmail.com>
2020-02-06 17:44:15 +0800

    SNMPServer: Listen To UDP

---
+++ interfaceListener.go
@@ -0,0 +1,75 @@
+package GoSNMPServer
+
+import "net"
+import "github.com/pkg/errors"
+
+type ISnmpServerListener interface {
+	SetupLogger(ILogger)
+	NextSnmp() (snmpbytes []byte, replayer IReplayer, err error)
+	Shutdown()
+}
+
+type IReplayer interface {
+	ReplayPDU([]byte) error
+	Shutdown()
+}
+
+type UDPListener struct {
+	conn   *net.UDPConn
+	logger ILogger
+}
+
+func NewUDPListener(l3proto, address string) (ISnmpServerListener, error) {
+	ret := new(UDPListener)
+	ret.logger = NewDiscardLogger()
+	udpaddr, err := net.ResolveUDPAddr(l3proto, address)
+	if err != nil {
+		return nil, errors.Wrap(err, "ResolveUDPAddr Error")
+	}
+	conn, err := net.ListenUDP(l3proto, udpaddr)
+	if err != nil {
+		return nil, errors.Wrap(err, "UDP Listen Error")
+	}
+	ret.conn = conn
+	return ret, nil
+}
+
+func (udp *UDPListener) SetupLogger(i ILogger) {
+	udp.logger = i
+}
+
+func (udp *UDPListener) NextSnmp() ([]byte, IReplayer, error) {
+	var msg [4096]byte
+	if udp.conn == nil {
+		return nil, nil, errors.New("Connection Not Listen")
+	}
+	counts, udpAddr, err := udp.conn.ReadFromUDP(msg[:])
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "UDP Read Error")
+	}
+	udp.logger.Infof("udp request from %v. size=%v", udpAddr, counts)
+	return msg[:counts], &UDPReplayer{udpAddr, udp.conn}, nil
+}
+
+func (udp *UDPListener) Shutdown() {
+	if udp.conn != nil {
+		udp.conn.Close()
+		udp.conn = nil
+	}
+}
+
+type UDPReplayer struct {
+	target *net.UDPAddr
+	conn   *net.UDPConn
+}
+
+func (r *UDPReplayer) ReplayPDU(i []byte) error {
+	conn := r.conn
+	_, err := conn.WriteToUDP(i, r.target)
+	if err != nil {
+		return errors.Wrap(err, "WriteToUDP")
+	}
+	return nil
+}
+
+func (r *UDPReplayer) Shutdown() {}
--- snmpserver.go
+++ snmpserver.go
@@ -1,38 +1,72 @@
 package GoSNMPServer
 
-import (
-	"fmt"
-	"net"
-)
+import "github.com/pkg/errors"
 
 type SNMPServer struct {
-	wconn net.Listener
+	wconnStream ISnmpServerListener
+	master      MasterAgent
+	logger      ILogger
 }
 
-func NewSNMPServer() *SNMPServer {
-	return new(SNMPServer)
-}
-
-func (server *SNMPServer) Listen(l3proto, address string) error {
-	if server.wconn != nil {
-		return fmt.Errorf("Already listened")
+func NewSNMPServer(master MasterAgent) *SNMPServer {
+	ret := new(SNMPServer)
+	if err := master.ReadyForWork(); err != nil {
+		panic(err)
 	}
-	ln, err := net.Listen(l3proto, address)
+	ret.master = master
+	ret.logger = master.Logger
+	return ret
+}
+
+func (server *SNMPServer) ListenUDP(l3proto, address string) error {
+	if server.wconnStream != nil {
+		return errors.New("Listened")
+	}
+	i, err := NewUDPListener(l3proto, address)
 	if err != nil {
-		// handle error
 		return err
 	}
-	server.wconn = ln
+	server.logger.Infof("ListenUDP: %s %s", l3proto, address)
+	i.SetupLogger(server.logger)
+	server.wconnStream = i
 	return nil
 }
 
-func (server *SNMPServer) ServeForever() {
-	for {
-		ln := server.wconn
-		_, err := ln.Accept()
-		if err != nil {
-			// handle error
-		}
-
+func (server *SNMPServer) ServeForever() error {
+	if server.wconnStream == nil {
+		return errors.New("Not Listen")
 	}
+
+	for {
+		err := server.ServeNextRequest()
+		if err != nil {
+			return errors.Wrap(err, "ServeNextRequest")
+		}
+	}
+}
+
+func (server *SNMPServer) ServeNextRequest() error {
+	bytePDU, replayer, err := server.wconnStream.NextSnmp()
+	if err != nil {
+		return err
+	}
+	result, err := server.master.ResponseForBuffer(bytePDU)
+	if err != nil {
+		v := "with"
+		if len(result) == 0 {
+			v = "without"
+		}
+		server.logger.Warnf("ResponseForBuffer Error: %v. %s result", err, v)
+	}
+	if len(result) != 0 {
+		if errreplay := replayer.ReplayPDU(result); errreplay != nil {
+			server.logger.Errorf("Replay PDU meet err:", errreplay)
+			replayer.Shutdown()
+			return nil
+		}
+	}
+	if err != nil {
+		replayer.Shutdown()
+	}
+	return nil
 }
