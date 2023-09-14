slayercat/GoSNMPServer

commit e5e512ec64b25cec8820baa669d9a270b1705ab0

bug:oidToByteString (#17)
* bug:oidToByteString

* bug:add verify oid at synconfig and change oidToByteString

* feat:change api

* add:comment

* fix:change verify,should uint32

* fix:modify toQuery

agentcontrolSubAgent.go
@@ -24,14 +24,21 @@ type SubAgent struct {
 }
 
 func (t *SubAgent) SyncConfig() error {
+	var (
+		err error
+	)
+	for _, oid := range t.OIDs {
+		if err = VerifyOid(oid.OID); err != nil {
+			return err
+		}
+	}
+	t.Logger.Debugf("Total OIDs of %v: %v", t.CommunityIDs, len(t.OIDs))
+
 	sort.Sort(byOID(t.OIDs))
-	t.Logger.Infof("Total OIDs of %v: %v", t.CommunityIDs, len(t.OIDs))
 	for id, each := range t.OIDs {
 		t.Logger.Infof("OIDs of %v: %v", t.CommunityIDs, each.OID)
 		if id != 0 && t.OIDs[id].OID == t.OIDs[id-1].OID {
-			verr := fmt.Sprintf("community %v: meet duplicate oid %v", t.CommunityIDs, each.OID)
-			t.Logger.Errorf(verr)
-			return errors.New(verr)
+			return fmt.Errorf("community %v: meet duplicate oid %v", t.CommunityIDs, each.OID)
 		}
 	}
 	return nil
@@ -442,16 +449,14 @@ func (t *SubAgent) serveSetRequest(i *gosnmp.SnmpPacket) (*gosnmp.SnmpPacket, er
 }
 
 func (t *SubAgent) getForPDUValueControl(oid string) (*PDUValueControlItem, int) {
-	toQuery := oidToByteString(oid)
+	// todo: maybe we can implement add "." at other place.
+	toQuery := "." + oidToByteString(oid)
 	i := sort.Search(len(t.OIDs), func(i int) bool {
 		thisOid := oidToByteString(t.OIDs[i].OID)
 		return thisOid >= toQuery
 	})
-	if i < len(t.OIDs) {
-		thisOid := oidToByteString(t.OIDs[i].OID)
-		if thisOid == toQuery {
-			return t.OIDs[i], i
-		}
+	if i < len(t.OIDs) && oidToByteString(t.OIDs[i].OID) == toQuery {
+		return t.OIDs[i], i
 	}
 	return nil, i
 }

go.sum
@@ -8,7 +8,6 @@ github.com/davecgh/go-spew v1.1.1 h1:vj9j/u1bqnvCEfJOwUhtlOARqs3+rkHYY13jYWTU97c
 github.com/davecgh/go-spew v1.1.1/go.mod h1:J7Y8YcW2NihsgmVo/mv3lAwl/skON4iLHjSsI+c5H38=
 github.com/go-ole/go-ole v1.2.4 h1:nNBDSCOigTSiarFpYE9J/KtEA1IOW4CNeqT9TQDqCxI=
 github.com/go-ole/go-ole v1.2.4/go.mod h1:XCwSNxSkXRo4vlyPy93sltvi/qJq0jqQhjqQNIwKuxM=
-github.com/golang/mock v1.2.0 h1:28o5sBqPkBsMGnC6b4MvE2TzSr5/AT4c/1fLqVGIwlk=
 github.com/golang/mock v1.2.0/go.mod h1:oTYuIxOrZwtPieC+H1uAHpcLFnEyAGVDL/k47Jfbm0A=
 github.com/google/go-cmp v0.3.1 h1:Xye71clBPdm5HgqGwUkwhbynsUJZhDbS20FvLhQ2izg=
 github.com/google/go-cmp v0.3.1/go.mod h1:8QqcDgzrUqlUb/G2PQTWiueGozuR1884gddMywk6iLU=
@@ -30,7 +29,6 @@ github.com/slayercat/gosnmp v1.24.1 h1:brqlcYbSEa5tESH+Dwo82Nm4Hnzt4pk1kTQ6Sxcl6
 github.com/slayercat/gosnmp v1.24.1/go.mod h1:EEciH24gj0Z8lijV/NUrlAZ8D1TYHImV0cvLMsUpRmM=
 github.com/stretchr/objx v0.1.0/go.mod h1:HFkY916IF+rwdDfMAkV7OtwuqBVzrE8GR6GFx+wExME=
 github.com/stretchr/testify v1.2.2/go.mod h1:a8OnRcib4nhh0OaRAV+Yts87kKdq0PP7pXfy6kDkUVs=
-github.com/stretchr/testify v1.3.0 h1:TivCn/peBQ7UY8ooIcPgZFpTNSz0Q2U6UrFlUfqbe0Q=
 github.com/stretchr/testify v1.3.0/go.mod h1:M5WIy9Dh21IEIfnGCwXGc5bZfKNJtfHm1UVUgZn+9EI=
 github.com/stretchr/testify v1.4.1-0.20200129000828-ea72eb91592e h1:diXFpsm7FZhZyMG+JS7uuehk3SmDKD8vfP6y8Jb9zMQ=
 github.com/stretchr/testify v1.4.1-0.20200129000828-ea72eb91592e/go.mod h1:5W2xD1RspED5o8YsWQXVCued0rvSQ+mT+I5cxcmMvtA=
@@ -42,7 +40,6 @@ golang.org/x/sys v0.0.0-20200124204421-9fbb57f87de9 h1:1/DFK4b7JH8DmkqhUk48onnSf
 golang.org/x/sys v0.0.0-20200124204421-9fbb57f87de9/go.mod h1:h1NjWce9XRLGQEsW7wpKNCjG9DtNlClVuFLEZdDNbEs=
 gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405 h1:yhCVgyC4o1eVCa2tZl7eS0r+SDo693bJlVdllGtEeKM=
 gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405/go.mod h1:Co6ibVJAznAaIkqp8huTwlJQCZ016jof/cbN4VW5Yz0=
-gopkg.in/yaml.v2 v2.2.2 h1:ZCJp+EgiOT7lHqUV2J862kp8Qj64Jo6az82+3Td9dZw=
 gopkg.in/yaml.v2 v2.2.2/go.mod h1:hI93XBmqTisBFMUTm0b8Fm+jr3Dg1NNxqwp+5A1VGuI=
 gopkg.in/yaml.v2 v2.4.0 h1:D8xgwECY7CYvx+Y2n4sBz93Jn9JRvxdiyyo8CTfuKaY=
 gopkg.in/yaml.v2 v2.4.0/go.mod h1:RDklbk79AGWmwhnvt/jBztapEOGDOx6ZbXqjP6csGnQ=

helpers.go
@@ -1,9 +1,11 @@
 package GoSNMPServer
 
-import "github.com/pkg/errors"
-import "github.com/slayercat/gosnmp"
-import "strings"
-import "strconv"
+import (
+	"github.com/pkg/errors"
+	"github.com/slayercat/gosnmp"
+	"strconv"
+	"strings"
+)
 
 func getPktContextOrCommunity(i *gosnmp.SnmpPacket) string {
 	if i.Version == gosnmp.Version3 {
@@ -21,28 +23,15 @@ func copySnmpPacket(i *gosnmp.SnmpPacket) gosnmp.SnmpPacket {
 	return ret
 }
 
+// Fix BUG: When converting certain byte values to the rune type,
+// some byte values may not be represented correctly because the rune type represents a Unicode character.
+// This can cause byte values to become unpredictable or incorrect after conversion.
 func oidToByteString(oid string) string {
-	xi := strings.Split(oid, ".")
-	out := []rune{}
-	for id, each := range xi {
-		if each == "" {
-			if id == 0 {
-				continue
-			} else {
-				panic(errors.Errorf("oidToByteString not valid id. value=%v", oid))
-			}
-
-		}
-		i, err := strconv.ParseInt(each, 10, 32)
-		if err != nil {
-			panic(err)
-		}
-		out = append(out, rune(i))
-	}
-	return string(out)
+	return oid
 }
 
-// IsValidObjectIdentifier will check a oid string is valid oid
+// IsValidObjectIdentifier will check an oid string is valid oid
+// Deprecated: instead use VerifyOid.
 func IsValidObjectIdentifier(oid string) (result bool) {
 	defer func() {
 		if err := recover(); err != nil {
@@ -55,4 +44,25 @@ func IsValidObjectIdentifier(oid string) (result bool) {
 	}
 	oidToByteString(oid)
 	return true
+}
+
+// VerifyOid will check an oid string is valid oid,
+// each number should be positive uint32.
+func VerifyOid(oid string) error {
+	xi := strings.Split(oid, ".")
+	for id, each := range xi {
+		if each == "" {
+			if id == 0 {
+				continue
+			}
+			return errors.New("oidToByteString not valid int,but it is empty " + oid)
+		}
+		i, err := strconv.ParseUint(each, 10, 32)
+		if err != nil {
+			return errors.New("oidToByteString not valid int. value=" + each)
+		} else if i < 0 {
+			return errors.New("oidToByteString not valid int. value=" + each + " should be positive")
+		}
+	}
+	return nil
 }

pducontrol.go
@@ -16,19 +16,21 @@ const PermissionAllowanceAllowed PermissionAllowance = 0
 // PermissionAllowanceDenied denies for access
 const PermissionAllowanceDenied PermissionAllowance = 1
 
-//FuncPDUControlCheckPermission checks for permission.
-//   return PermissionAllowanceAllowed / PermissionAllowanceDenied
+// FuncPDUControlCheckPermission checks for permission.
+//
+//	return PermissionAllowanceAllowed / PermissionAllowanceDenied
 type FuncPDUControlCheckPermission func(pktVersion gosnmp.SnmpVersion, pduType gosnmp.PDUType, contextName string) PermissionAllowance
 
-//FuncPDUControlTrap will be called on trap.
-//    args:
-//		isInform: indicate if the request is a InformRequest.
-//          true  -- It's a InformRequest. data will be returns to the client
-//			false -- It's a trap.  data to returned will drop silencely.
-// 		trapdata: what client asks for.
-//    returns:
-//		dataret -- try to return to client. nil for nothing to return
-//		err  --  any error?(will return to client by string)
+// FuncPDUControlTrap will be called on trap.
+//
+//	   args:
+//			isInform: indicate if the request is a InformRequest.
+//	         true  -- It's a InformRequest. data will be returns to the client
+//				false -- It's a trap.  data to returned will drop silencely.
+//			trapdata: what client asks for.
+//	   returns:
+//			dataret -- try to return to client. nil for nothing to return
+//			err  --  any error?(will return to client by string)
 type FuncPDUControlTrap func(isInform bool, trapdata gosnmp.SnmpPDU) (dataret interface{}, err error)
 
 // FuncPDUControlGet will be called on get value
@@ -123,9 +125,7 @@ func (x byOID) Len() int {
 }
 
 func (x byOID) Less(i, j int) bool {
-	stripedI := oidToByteString(x[i].OID)
-	stripedJ := oidToByteString(x[j].OID)
-	return stripedI < stripedJ
+	return oidToByteString(x[i].OID) < oidToByteString(x[j].OID)
 }
 
 func (x byOID) Swap(i, j int) {

commit f220d18995267b7e1ad44a23fb1239e6741eeaa1

Fixed sorting and lookup errors when the OID part >= 10.
The commit e5e512ec64b25cec8820baa669d9a270b1705ab0 inadvertently introduced a
lookup error while fixing the issue.

When the OID is greater than or equal to 10, inconsistencies between the string
lookup method and the expected behavior resulted in disorder and lookup failures.

By introducing the ByteString type to represent the internal form of the OID,
sorting and binary search now behave as expected.

agentcontrolSubAgent.go
@@ -449,14 +449,19 @@ func (t *SubAgent) serveSetRequest(i *gosnmp.SnmpPacket) (*gosnmp.SnmpPacket, er
 }
 
 func (t *SubAgent) getForPDUValueControl(oid string) (*PDUValueControlItem, int) {
-	// todo: maybe we can implement add "." at other place.
-	toQuery := "." + oidToByteString(oid)
+	toQuery := oidToByteString(oid)
 	i := sort.Search(len(t.OIDs), func(i int) bool {
 		thisOid := oidToByteString(t.OIDs[i].OID)
-		return thisOid >= toQuery
+		compareResult := compareByteString(thisOid, toQuery)
+		// thisOid >= toQuery
+		return compareResult == ByteStringCompareResultGreaterThen || compareResult == ByteStringCompareResultEqual
 	})
-	if i < len(t.OIDs) && oidToByteString(t.OIDs[i].OID) == toQuery {
-		return t.OIDs[i], i
+
+	if i < len(t.OIDs) {
+		// t.OIDs[i].OID == toQuery
+		if compareByteString(oidToByteString(t.OIDs[i].OID), toQuery) == ByteStringCompareResultEqual {
+			return t.OIDs[i], i
+		}
 	}
 	return nil, i
 }

go.mod
@@ -8,10 +8,9 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/procfs v0.0.8
 	github.com/shirou/gopsutil v2.19.12+incompatible
-	github.com/sirupsen/logrus v1.8.1
-	github.com/slayercat/gosnmp v1.24.1
+	github.com/sirupsen/logrus v1.4.2
+	github.com/slayercat/gosnmp v1.24.1-0.20200225080903-29819b7774e5
 	github.com/stretchr/testify v1.4.1-0.20200129000828-ea72eb91592e
 	github.com/urfave/cli/v2 v2.1.1
 	golang.org/x/sys v0.0.0-20200124204421-9fbb57f87de9 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 )

go.sum
@@ -11,6 +11,8 @@ github.com/go-ole/go-ole v1.2.4/go.mod h1:XCwSNxSkXRo4vlyPy93sltvi/qJq0jqQhjqQNI
 github.com/golang/mock v1.2.0/go.mod h1:oTYuIxOrZwtPieC+H1uAHpcLFnEyAGVDL/k47Jfbm0A=
 github.com/google/go-cmp v0.3.1 h1:Xye71clBPdm5HgqGwUkwhbynsUJZhDbS20FvLhQ2izg=
 github.com/google/go-cmp v0.3.1/go.mod h1:8QqcDgzrUqlUb/G2PQTWiueGozuR1884gddMywk6iLU=
+github.com/konsorten/go-windows-terminal-sequences v1.0.1 h1:mweAR1A6xJ3oS2pRaGiHgQ4OO8tzTaLawm8vnODuwDk=
+github.com/konsorten/go-windows-terminal-sequences v1.0.1/go.mod h1:T0+1ngSBFLxvqU3pZ+m/2kptfBszLMUkC4ZK/EgS/cQ=
 github.com/pkg/errors v0.9.1 h1:FEBLx1zS214owpjy7qsBeixbURkuhQAwrK5UwLGTwt4=
 github.com/pkg/errors v0.9.1/go.mod h1:bwawxfHBFNV+L2hUp1rHADufV3IMtnDRdf1r5NINEl0=
 github.com/pmezard/go-difflib v1.0.0 h1:4DBwDE0NGyQoBHbLQYPwSUPoCMWR5BEzIk/f1lZbAQM=
@@ -23,11 +25,16 @@ github.com/shirou/gopsutil v2.19.12+incompatible h1:WRstheAymn1WOPesh+24+bZKFkqr
 github.com/shirou/gopsutil v2.19.12+incompatible/go.mod h1:5b4v6he4MtMOwMlS0TUMTu2PcXUg8+E1lC7eC3UO/RA=
 github.com/shurcooL/sanitized_anchor_name v1.0.0 h1:PdmoCO6wvbs+7yrJyMORt4/BmY5IYyJwS/kOiWx8mHo=
 github.com/shurcooL/sanitized_anchor_name v1.0.0/go.mod h1:1NzhyTcUVG4SuEtjjoZeVRXNmyL/1OwPU0+IJeTBvfc=
+github.com/sirupsen/logrus v1.4.2 h1:SPIRibHv4MatM3XXNO2BJeFLZwZ2LvZgfQ5+UNI2im4=
+github.com/sirupsen/logrus v1.4.2/go.mod h1:tLMulIdttU9McNUspp0xgXVQah82FyeX6MwdIuYE2rE=
 github.com/sirupsen/logrus v1.8.1 h1:dJKuHgqk1NNQlqoA6BTlM1Wf9DOH3NBjQyu0h9+AZZE=
 github.com/sirupsen/logrus v1.8.1/go.mod h1:yWOB1SBYBC5VeMP7gHvWumXLIWorT60ONWic61uBYv0=
+github.com/slayercat/gosnmp v1.24.1-0.20200225080903-29819b7774e5 h1:Zq1YuJvWWpmrHqdIBK95DFgI7+pZXL/zejuLxRljL2I=
+github.com/slayercat/gosnmp v1.24.1-0.20200225080903-29819b7774e5/go.mod h1:tB/loGhNgiSxOZzz02TRKKhUPUzDrGrkYQSXxD3KfqM=
 github.com/slayercat/gosnmp v1.24.1 h1:brqlcYbSEa5tESH+Dwo82Nm4Hnzt4pk1kTQ6Sxcl68w=
 github.com/slayercat/gosnmp v1.24.1/go.mod h1:EEciH24gj0Z8lijV/NUrlAZ8D1TYHImV0cvLMsUpRmM=
 github.com/stretchr/objx v0.1.0/go.mod h1:HFkY916IF+rwdDfMAkV7OtwuqBVzrE8GR6GFx+wExME=
+github.com/stretchr/objx v0.1.1/go.mod h1:HFkY916IF+rwdDfMAkV7OtwuqBVzrE8GR6GFx+wExME=
 github.com/stretchr/testify v1.2.2/go.mod h1:a8OnRcib4nhh0OaRAV+Yts87kKdq0PP7pXfy6kDkUVs=
 github.com/stretchr/testify v1.3.0/go.mod h1:M5WIy9Dh21IEIfnGCwXGc5bZfKNJtfHm1UVUgZn+9EI=
 github.com/stretchr/testify v1.4.1-0.20200129000828-ea72eb91592e h1:diXFpsm7FZhZyMG+JS7uuehk3SmDKD8vfP6y8Jb9zMQ=
@@ -35,11 +42,13 @@ github.com/stretchr/testify v1.4.1-0.20200129000828-ea72eb91592e/go.mod h1:5W2xD
 github.com/urfave/cli/v2 v2.1.1 h1:Qt8FeAtxE/vfdrLmR3rxR6JRE0RoVmbXu8+6kZtYU4k=
 github.com/urfave/cli/v2 v2.1.1/go.mod h1:SE9GqnLQmjVa0iPEY0f1w3ygNIYcIJ0OKPMoW2caLfQ=
 golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e/go.mod h1:RxMgew5VJxzue5/jJTE5uejpjVlOe/izrB70Jof72aM=
+golang.org/x/sys v0.0.0-20190422165155-953cdadca894/go.mod h1:h1NjWce9XRLGQEsW7wpKNCjG9DtNlClVuFLEZdDNbEs=
 golang.org/x/sys v0.0.0-20191026070338-33540a1f6037/go.mod h1:h1NjWce9XRLGQEsW7wpKNCjG9DtNlClVuFLEZdDNbEs=
 golang.org/x/sys v0.0.0-20200124204421-9fbb57f87de9 h1:1/DFK4b7JH8DmkqhUk48onnSfrPzImPoVxuomtbT2nk=
 golang.org/x/sys v0.0.0-20200124204421-9fbb57f87de9/go.mod h1:h1NjWce9XRLGQEsW7wpKNCjG9DtNlClVuFLEZdDNbEs=
 gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405 h1:yhCVgyC4o1eVCa2tZl7eS0r+SDo693bJlVdllGtEeKM=
 gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405/go.mod h1:Co6ibVJAznAaIkqp8huTwlJQCZ016jof/cbN4VW5Yz0=
+gopkg.in/yaml.v2 v2.2.2 h1:ZCJp+EgiOT7lHqUV2J862kp8Qj64Jo6az82+3Td9dZw=
 gopkg.in/yaml.v2 v2.2.2/go.mod h1:hI93XBmqTisBFMUTm0b8Fm+jr3Dg1NNxqwp+5A1VGuI=
 gopkg.in/yaml.v2 v2.4.0 h1:D8xgwECY7CYvx+Y2n4sBz93Jn9JRvxdiyyo8CTfuKaY=
 gopkg.in/yaml.v2 v2.4.0/go.mod h1:RDklbk79AGWmwhnvt/jBztapEOGDOx6ZbXqjP6csGnQ=

helpers.go
@@ -23,11 +23,58 @@ func copySnmpPacket(i *gosnmp.SnmpPacket) gosnmp.SnmpPacket {
 	return ret
 }
 
+type ByteString []int
+type ByteStringCompareResult int
+
+const ByteStringCompareResultEqual = 0
+const ByteStringCompareResultLessThen = -1
+const ByteStringCompareResultGreaterThen = 1
+
+func compareByteString(arr1, arr2 ByteString) ByteStringCompareResult {
+	minLength := len(arr1)
+	if len(arr2) < minLength {
+		minLength = len(arr2)
+	}
+
+	for i := 0; i < minLength; i++ {
+		if arr1[i] < arr2[i] {
+			return ByteStringCompareResultLessThen
+		} else if arr1[i] > arr2[i] {
+			return ByteStringCompareResultGreaterThen
+		}
+	}
+
+	if len(arr1) < len(arr2) {
+		return ByteStringCompareResultLessThen
+	} else if len(arr1) > len(arr2) {
+		return ByteStringCompareResultGreaterThen
+	}
+
+	return ByteStringCompareResultEqual
+}
+
 // Fix BUG: When converting certain byte values to the rune type,
 // some byte values may not be represented correctly because the rune type represents a Unicode character.
 // This can cause byte values to become unpredictable or incorrect after conversion.
-func oidToByteString(oid string) string {
-	return oid
+func oidToByteString(oid string) ByteString {
+	xi := strings.Split(oid, ".")
+	out := []int{}
+	for id, each := range xi {
+		if each == "" {
+			if id == 0 {
+				continue
+			} else {
+				panic(errors.Errorf("oidToByteString not valid id. value=%v", oid))
+			}
+
+		}
+		i, err := strconv.ParseInt(each, 10, 32)
+		if err != nil {
+			panic(err)
+		}
+		out = append(out, int(i))
+	}
+	return ByteString(out)
 }
 
 // IsValidObjectIdentifier will check an oid string is valid oid

pducontrol.go
@@ -125,7 +125,11 @@ func (x byOID) Len() int {
 }
 
 func (x byOID) Less(i, j int) bool {
-	return oidToByteString(x[i].OID) < oidToByteString(x[j].OID)
+	bsi := oidToByteString(x[i].OID)
+	bsj := oidToByteString(x[j].OID)
+	//  oidToByteString(x[i].OID) < oidToByteString(x[j].OID)
+	return compareByteString(bsi, bsj) == ByteStringCompareResultLessThen
+
 }
 
 func (x byOID) Swap(i, j int) {